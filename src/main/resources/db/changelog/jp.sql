531acdde583a4f9ac165db3dc00b858690e7e7cf
jayaprakash7992 <jayaprakash7992@gmail.com>
2019-11-19 19:50:07 +0530

    Update jp.sql

--- src/main/resources/db/changelog/jp.sql
+++ src/main/resources/db/changelog/jp.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 
 --changeset jayp:change_1
-INSERT INTO books(id, name, author) VALUES (1, "DevOpsBook", "JayP");
+INSERT INTO books(id, name, author) VALUES(1, "DevOpsBook", "JayP");
